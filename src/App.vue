1d27c3f41e8bd129c171d251a45c1194114e0161
indinner <520244@qq.com>
2024-04-26 15:35:39 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <div style="position:fixed;z-index: 9;left: 0;top: 0;background-color: #f5f5f5;width: 20vw;height: 10vh;display: flex;align-items: center;border-bottom-right-radius: 20px">
+      <img style="width: 90%;margin-left: 5%;" src="https://cdn.indinner.com/icon/logo.png">
+    </div>
+    <div style="overflow: hidden;width: 100vw;height: 100vh">
+      <iframe frameborder="no" allow='microphone;camera;' style="width: 100vw;height: 100vh;overflow: hidden" src="https://train.aimaker.space/train"></iframe>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
